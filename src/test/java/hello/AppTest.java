b7a242527610b0c92a3a4d27617372d17bce10c0
taixingbi <bitaihang@gmail.com>
2021-09-26 10:07:25 -0400

    first commit

---
+++ src/test/java/hello/AppTest.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void main(){
+        System.out.println("----------------test------------------");
+        System.out.println("----------------test end------------------");
+    }
+
+}
